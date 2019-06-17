b797c1130154387e3a00d29ab55e6658e4196a52
RabirajB <31617556+RabirajB@users.noreply.github.com>
2019-06-17 19:40:36 +0530

    Add files via upload
    New changes comitted

--- constriction_factor_pso.py
+++ constriction_factor_pso.py
@@ -1,5 +1,7 @@
 #program to demonstrate constriction factor based particle swarm optimization algorithm
 import random
+from ctypes import c_byte
+
 import gridgraphdemo as gd
 import prim_algorithm as pa
 from functools import reduce
@@ -19,23 +21,15 @@ class Particle:
         self.Xp = Xp
         self.Yp = Yp
 
-def get_min_item(arr):
-    min = math.inf
-    index = 0
-    for i in range(len(arr)):
-        if arr[i] < min:
-            min = arr[i]
-            index = i
-    item = Min_Item(min, index)
-    return item
+
 
 def constriction_factor_particle_swarm_optimization(objgbest, p, objpbest):
     gbest = math.ceil(1/objgbest)
     pbest = math.ceil(1/objpbest)
     k = 0.729
-    Vx = k*(p.V + 2.05 * random.randint(0,1) * (pbest - p.Xp) + 2 * random.randint (0,1) * (gbest - p.Xp))
+    Vx = k*(p.V + 2 * random.randint(0,1) * (pbest - p.Xp) + 2 * random.randint (0,1) * (gbest - p.Xp))
     #print(Vx)
-    Vy = k*(p.V + 2.05 * random.randint(0,1) * (pbest - p.Yp) + 2 * random.randint (0,1) * (gbest - p.Yp))
+    Vy = k*(p.V + 2 * random.randint(0,1) * (pbest - p.Yp) + 2 * random.randint (0,1) * (gbest - p.Yp))
     #print(Vy)
     Vnew = math.ceil((Vx + Vy))//2
     Sxnew = list(map(lambda x: x + Vx, p.Sx))
@@ -67,29 +61,81 @@ def get_fitness(Tx, Ty, particle):
 
 def constriction_factor_particle_swarm_test(Tx, Ty):
     particles = []
-    global objfitness
-    n = random.randint(0,10)
+    pbestvector = []
+    lenTx = len(Tx)
+    lenTy = len(Ty)
+    # global objfitness
+    n = random.randint(0, 10)
     for i in range(n):
         Sx = gd.get_xdata(0, 500, 98)
         Sy = gd.get_ydata(0, 500, 98)
-        Xs = reduce((lambda x, y: x+y), Sx) // len(Sx)
-        Ys = reduce((lambda x, y: x+y), Sy) // len(Sy)
+        Xs = reduce((lambda x, y: x + y), Sx) // len(Sx)
+        Ys = reduce((lambda x, y: x + y), Sy) // len(Sy)
         particle = Particle(Sx, Sy, 0, Xs, Ys)
         particles.append(particle)
+        pbestvector.append(math.inf)
     for i in range(20):
-        objfitness = []
+        # objfitness = []
+
         for j in range(len(particles)):
             mst = get_fitness(Tx, Ty, particles[j])
-            objfitness.append(mst)
-            Tx = Tx[0:len(Tx) - len(particles[j].Sx)]
-            Ty = Ty[0:len(Ty) - len(particles[j].Sy)]
-        best_obj_fitness = get_min_item(objfitness).minitem
+            if mst < pbestvector[j]:
+                pbestvector[j] = mst
+            Rx = Tx[0:len(Tx) - lenTx]
+            Ry = Tx[0:len(Tx) - lenTy]
+            Tx = Tx[0:len(Tx) - len(Rx)]
+            Ty = Tx[0:len(Ty) - len(Ry)]
+        best_obj_fitness = min(pbestvector)
         for j in range(len(particles)):
-            constriction_factor_particle_swarm_optimization(best_obj_fitness, particles[j], objfitness[j])
-            print(particles[j].Sx)
-            print(particles[j].Sy)
-    particle_best = particles[get_min_item(objfitness).item_id]
+            constriction_factor_particle_swarm_optimization(best_obj_fitness, particles[j], pbestvector[j])
+
+    particle_best = particles[pbestvector.index(min(pbestvector))]
     return particle_best
+
+
+def call_methods(Tx, Ty, lenTx, lenTy):
+    # Tx = gd.get_xdata(0, 500, 100)
+    # Ty = gd.get_ydata(0, 500, 100)
+    '''
+    lenTx = len(Tx)
+    lenTy = len(Ty)
+    '''
+    # Calculating mST for CPSO
+    Rx = Tx[0:len(Tx) - lenTx]
+    Ry = Ty[0:len(Tx) - lenTy]
+    Tx = Tx[0:len(Tx) - len(Rx)]
+    Ty = Ty[0:len(Ty) - len(Ry)]
+    print("X coordinates =", Tx)
+    print("Y coordinates =", Ty)
+    bestparticle = constriction_factor_particle_swarm_test(Tx, Ty)
+    Rx = Tx[0:len(Tx) - lenTx]
+    Ry = Ty[0:len(Ty) - lenTy]
+    Tx = Tx[0:len(Tx) - len(Rx)]
+    Ty = Ty[0:len(Ty) - len(Ry)]
+    # print("Updated X coordinates", Tx)
+    # print("Updated Y coordinates", Ty)
+    print("X coordinates of best particle", bestparticle.Sx)
+    print("Y coordinates of best particle", bestparticle.Sy)
+    print("Updated X coordinates", Tx)
+    print("Updated Y Coordinates", Ty)
+    count = 0
+    for i in range(len(bestparticle.Sx)):
+        if bestparticle.Sx[i] < min(Tx) or bestparticle.Sx[i] > max(Tx):
+            continue
+        if bestparticle.Sy[i] < min(Ty) or bestparticle.Sy[i] > max(Ty):
+            continue
+
+        Tx.append(bestparticle.Sx[i])
+        Ty.append(bestparticle.Sy[i])
+        count = count + 1
+    print("Updated X Coordinates", Tx)
+    print("Updated Y Coordinates", Ty)
+    distancevector = gd.get_distancevector(Tx, Ty)
+    mst = pa.mst_prim(distancevector)
+    mst_size = pa.get_tree(distancevector)
+    print("Size of Steiner Tree for Constricted-PSO", mst_size)
+    pa.draw_gridgraph(Tx, Ty, mst)
+
 
 
 
--- pigeon_optimization_algorithm.py
+++ pigeon_optimization_algorithm.py
@@ -1,48 +1,35 @@
-#program to demonstrate pigeon optimization algorithm
+# program to demonstrate pigeon optimization algorithm
 import random
 import gridgraphdemo as gd
 import prim_algorithm as pa
 from functools import reduce
 import math
 
-class Min_Item:
-    def __init__(self, minitem,  item_id):
-        self.minitem = minitem
-        self.item_id = item_id
 
 class Pigeon:
-    #class used to initialize a pigeon
-    def __init__(self,Sx,Sy,V,Xp,Yp):
+    # class used to initialize a pigeon
+    def __init__(self, Sx, Sy, V, Xp, Yp):
         self.Sx = Sx
         self.Sy = Sy
         self.V = V
         self.Xp = Xp
         self.Yp = Yp
 
-def get_min_item(arr):
-    min = math.inf
-    index = 0
-    for i in range(len(arr)):
-        if arr[i] < min:
-            min = arr[i]
-            index = i
-    item = Min_Item(min, index)
-    return item
 
-def pigeon_optimization(Objbest,p,iteration):
-    fitnessbest = math.ceil(1/Objbest)
-    Vx = p.V * 3**(random.randint(0,1)*iteration) + random.randint(0, 1) * (fitnessbest- p.Xp)
-    #print("x velocity=",Vx)
-    Vy = p.V * 3**(random.randint(0,1)*iteration) + random.randint(0, 1) * (fitnessbest- p.Yp)
-    #print("Y velocity",Vy)
-    Vnew = (Vx + Vy)//2
+def pigeon_optimization(Objbest, p, iteration):
+    fitnessbest = math.ceil(1 / Objbest)
+    Vx = p.V * math.e ** (random.randint(0, 1) * iteration) + random.randint(0, 1) * (fitnessbest - p.Xp)
+    # print("x velocity=",Vx)
+    Vy = p.V * math.e ** (random.randint(0, 1) * iteration) + random.randint(0, 1) * (fitnessbest - p.Yp)
+    # print("Y velocity",Vy)
+    Vnew = math.ceil((Vx + Vy)) // 2
     p.V = Vnew
 
     Sxnew = list(map(lambda x: x + Vx, p.Sx))
     Synew = list(map(lambda y: y + Vy, p.Sy))
-    Xpnew = reduce((lambda x,y: x+y),Sxnew) // len(p.Sx)
-    Ypnew = reduce((lambda x, y: x + y),Synew) // len(p.Sy)
-    if Xpnew < 0 or Xpnew > 500 or Ypnew < 0 or Ypnew >500:
+    Xpnew = reduce((lambda x, y: x + y), Sxnew) // len(p.Sx)
+    Ypnew = reduce((lambda x, y: x + y), Synew) // len(p.Sy)
+    if Xpnew < 0 or Xpnew > 500 or Ypnew < 0 or Ypnew > 500:
         p.Sx = p.Sx
         p.Sy = p.Sy
         p.V = p.V
@@ -55,66 +42,99 @@ def pigeon_optimization(Objbest,p,iteration):
         p.Xp = Xpnew
         p.Yp = Ypnew
 
+
 def get_fitness(Tx, Ty, pigeon):
     for i in range(len(pigeon.Sx)):
+        if pigeon.Sx[i] < min(Tx) or pigeon.Sx[i] > max(Tx):
+            continue
+        if pigeon.Sy[i] < min(Ty) or pigeon.Sy[i] > max(Ty):
+            continue
         Tx.append(pigeon.Sx[i])
         Ty.append(pigeon.Sy[i])
+
     distancevector = gd.get_distancevector(Tx, Ty)
     objectivefitness = pa.get_tree(distancevector)
     return objectivefitness
 
-def pigeon_test(Tx,Ty):
+
+def pigeon_test(Tx, Ty):
     pigeons = []
     global objfitness
-    n = random.randint(0,10)
+    n = random.randint(0, 10)
+    lenTx = len(Tx)
+    lenTy = len(Ty)
     for i in range(n):
-        Sx = gd.get_xdata(0,500, 98)
-        Sy = gd.get_ydata(0,500, 98)
-        Xs = reduce((lambda x,y: x+y), Sx) // len(Sx)
-        Ys = reduce((lambda x,y: x+y), Sy) // len(Sy)
+        Sx = gd.get_xdata(0, 500, 98)
+        Sy = gd.get_ydata(0, 500, 98)
+        Xs = reduce((lambda x, y: x + y), Sx) // len(Sx)
+        Ys = reduce((lambda x, y: x + y), Sy) // len(Sy)
         pigeon = Pigeon(Sx, Sy, 0, Xs, Ys)
         pigeons.append(pigeon)
 
     for i in range(20):
         objfitness = []
         for j in range(len(pigeons)):
-            mst = get_fitness(Tx,Ty,pigeons[j])
+            mst = get_fitness(Tx, Ty, pigeons[j])
             objfitness.append(mst)
-            Tx = Tx[0:len(Tx) - len(pigeons[j].Sx)]
-            #print(Tx)
-            Ty = Ty[0:len(Ty) - len(pigeons[j].Sy)]
-            #print(Ty)
-        best_obj_fitness = get_min_item(objfitness).minitem
+            Rx = Tx[0:len(Tx) - lenTx]
+            Ry = Tx[0:len(Tx) - lenTy]
+            Tx = Tx[0:len(Tx) - len(Rx)]
+            Ty = Tx[0:len(Ty) - len(Ry)]
+            # Tx = Tx[0:len(Tx) - len(pigeons[j].Sx)]
+            # print(Tx)
+            # Ty = Ty[0:len(Ty) - len(pigeons[j].Sy)]
+            # print(Ty)
+        best_obj_fitness = min(objfitness)
 
         for j in range(len(pigeons)):
-
             pigeon_optimization(best_obj_fitness, pigeons[j], i)
 
-    pigeon_best = pigeons[get_min_item(objfitness).item_id]
+    pigeon_best = pigeons[objfitness.index(min(objfitness))]
     print(pigeon_best.Sx)
     print(pigeon_best.Sy)
-    print(Tx)
-    print(Ty)
     return pigeon_best
 
 
+def call_methods(Tx, Ty, lenTx, lenTy):
+    # Tx = gd.get_xdata(0, 500, 10)
+    # Ty = gd.get_ydata(0, 500, 10)
+    '''
+    lenTx = len(Tx)
+    lenTy = len(Ty)
+    '''
+    # Calculating mST for PIO
+    print("X coordinates =", Tx)
+    print("Y coordinates =", Ty)
+    bestpigeon = pigeon_test(Tx, Ty)
+    Rx = Tx[0:len(Tx) - lenTx]
+    Ry = Ty[0:len(Ty) - lenTy]
+    Tx = Tx[0:len(Tx) - len(Rx)]
+    Ty = Ty[0:len(Ty) - len(Ry)]
+    # print("Updated X coordinates", Tx)
+    # print("Updated Y coordinates", Ty)
+    print("X coordinates of best pigeon", bestpigeon.Sx)
+    print("Y coordinates of best pigeon", bestpigeon.Sy)
+    print("Updated X coordinates", Tx)
+    print("Updated Y Coordinates", Ty)
+    count = 0
+    for i in range(len(bestpigeon.Sx)):
+        if bestpigeon.Sx[i] < min(Tx) or bestpigeon.Sx[i] > max(Tx):
+            continue
+        if bestpigeon.Sy[i] < min(Ty) or bestpigeon.Sy[i] > max(Ty):
+            continue
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        Tx.append(bestpigeon.Sx[i])
+        Ty.append(bestpigeon.Sy[i])
+        count = count + 1
+    print("Updated X Coordinates", Tx)
+    print("Updated Y Coordinates", Ty)
+    distancevector = gd.get_distancevector(Tx, Ty)
+    mst = pa.mst_prim(distancevector)
+    mst_size = pa.get_tree(distancevector)
+    print("Size of Steiner Tree for PIO", mst_size)
+    pa.draw_gridgraph(Tx, Ty, mst)
+    Tx = Tx[0:len(Tx) - count]
+    Ty = Ty[0:len(Ty) - count]
+    print("Restored X Coordinates=", Tx)
+    print("Restored Y Coordinates=", Ty)
+    print("End of PIO")
