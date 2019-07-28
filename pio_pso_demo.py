c3e2455d6f3532d4b742141e5a9ad77d6b48548b
RabirajB <31617556+RabirajB@users.noreply.github.com>
2019-07-28 12:41:05 +0530

    Add files via upload

--- pio_pso_demo.py
+++ pio_pso_demo.py
@@ -3,11 +3,15 @@ import gridgraphdemo as gd
 import pigeon_optimization_algorithm as poa
 import particle_swarm_optimization as pso
 import constriction_factor_pso as cpso
+import invasive_weed_optimization as iwo
+import numpy as np
 
-Tx = gd.get_xdata(0, 500, 20) #Change values over here and test
-Ty = gd.get_ydata(0, 500, 20) #Change values over here and test
-lenTx = len(Tx)
-lenTy = len(Ty)
+Tx, Ty = np.load("terminal_point_{}_{}.npy".format(str(500), str(30)))
+#Ty = gd.get_ydata(0, 500, 20)
+#lenTx = len(Tx)
+#lenTy = len(Ty)
+lenTx = Tx.size
+lenTy = lenTx
 
 print("X Coordinates", Tx)
 print("Y Coordinates", Ty)
@@ -22,3 +26,6 @@ poa.call_methods(Tx,Ty,lenTx,lenTy)
 pso.call_methods(Tx,Ty,lenTx,lenTy)
 
 cpso.call_methods(Tx,Ty,lenTx,lenTy)
+iwo.iwo_test(Tx,Ty)
+
+
