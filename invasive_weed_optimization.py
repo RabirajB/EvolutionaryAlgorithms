a74b87607de34da6fcd1976bd8f66734b39454a3
Sapi98 <31105656+Sapi98@users.noreply.github.com>
2019-08-06 23:44:30 +0530

    Automation
    automation added and minor changes done. genreate_static_dataset.py file added.

--- invasive_weed_optimization.py
+++ invasive_weed_optimization.py
@@ -5,7 +5,7 @@ from functools import reduce
 import numpy as np
 
 class Weed:
-    def __init__(self,Wx,Wy,Xp,Yp,fitness):
+    def __init__(self, Wx, Wy, Xp, Yp, fitness):
         self.Wx = Wx
         self.Wy = Wy
         self.Xp = Xp
@@ -18,6 +18,7 @@ class Seed:
         self.Sy = Sy
         self.Xp = Xp
         self.Yp = Yp
+
 def get_sum(weedlist):
     sum = 0
     for w in weedlist:
@@ -26,17 +27,21 @@ def get_sum(weedlist):
 
 def get_max_fitness(weedlist):
     fmax = 0
+    weed = None
     for i in range(len(weedlist)):
         if weedlist[i].fitness > fmax:
             fmax = weedlist[i].fitness
-    return fmax
+            weed = weedlist[i]
+    return (fmax, weed)
 
 def get_min_fitness(weedlist):
     fmin = 9999
+    weed = None
     for i in range(len(weedlist)):
         if weedlist[i].fitness > fmin:
             fmin = weedlist[i].fitness
-    return fmin
+            weed = weedlist[i]
+    return (fmin, weed)
 
 def get_fitness(Tx,Ty,weed):
     for i in range(len(weed.Wx)):
@@ -44,56 +49,77 @@ def get_fitness(Tx,Ty,weed):
             continue
         if weed.Wy[i] < min(Ty) or weed.Wy[i] > max(Ty):
             continue
-        Tx.append(weed.Wx[i])
-        Ty.append(weed.Wy[i])
+        #Tx.append(weed.Wx[i])
+        #Ty.append(weed.Wy[i])
+
+        Tx = np.append(Tx, weed.Wx[i])
+        Ty = np.append(Ty, weed.Wy[i])
 
     distancevector = gd.get_distancevector(Tx, Ty)
     objectivefitness = pa.get_tree(distancevector)
     return objectivefitness
 
 def competitive_exclusion(weedlist):
+
     meanfitness = get_sum(weedlist) // len(weedlist)
     for weed in weedlist:
         if weed.fitness < meanfitness:
             weedlist.remove(weed)
 
-def iwo_algorithm(Tx,Ty,weedlist,itermax,pmax,Smax,Smin,sigmafinal,sigmainit,n):# n = modulation index
-   global fmax,fmin
-   lenTx = len(Tx)
-   lenTy = len(Ty)
-   for i in range(1,itermax+1):
+def iwo_algorithm(Tx,Ty,weedlist,itermax,pmax,Smax,Smin,sigmafinal,sigmainit,n): # n = modulation index
+   #global fmax,fmin
+   #lenTx = len(Tx)
+   #lenTy = len(Ty)
+   lenTx = Tx.size
+   lenTy = lenTx
+   for i in range(1, itermax+1):
         for w in weedlist:
-            w.fitness = (1/get_fitness(Tx,Ty,w))
-            Rx = Tx[0:len(Tx) - lenTx]
-            Ry = Ty[0:len(Ty) - lenTy]
-            Tx = Tx[0:len(Tx) - len(Rx)]
-            Ty = Ty[0:len(Ty) - len(Ry)]
+            w.fitness = (1/get_fitness(Tx, Ty, w))
+            #Rx = Tx[0:len(Tx) - lenTx]
+            #Ry = Ty[0:len(Ty) - lenTy]
+            #Tx = Tx[0:len(Tx) - len(Rx)]
+            #Ty = Ty[0:len(Ty) - len(Ry)]
+            temp_len = Tx.size
+            Rx = Tx[0:temp_len - lenTx]
+            Ry = Ty[0:temp_len - lenTy]
+            len_Rx = Rx.size
+            Tx = Tx[0:temp_len - len_Rx]
+            Ty = Ty[0:temp_len - len_Rx]
         # Convergence
         component = ((itermax - i) / itermax) ** n
         sigmaiter = sigmafinal + (sigmainit - sigmafinal) * component
-        fmax = get_max_fitness(weedlist)
-        fmin = get_min_fitness(weedlist)
+        fmax, _ = get_max_fitness(weedlist)
+        fmin, _ = get_min_fitness(weedlist)
         for weed in weedlist:
             Splant = Smin + math.ceil(weed.fitness * (Smax - Smin)/(fmax - fmin))
             seedlist = []
             # Initiation
-            for k in range(Splant):
-                Sx = np.random.normal(weed.Xp, sigmaiter, 8) # Skew Position
-                print(Sx)
-                Sy = np.random.normal(weed.Yp, sigmaiter, 8)
-                print(Sy)
-                Xp = reduce((lambda x, y: x+y), Sx)//len(Sx)
-                Yp = reduce((lambda x, y: x+y), Sy)//len(Sy)
+            for j in range(Splant):
+                Sx = np.random.normal(weed.Xp, sigmaiter, lenTx - 2)  # Skew Position
+                #print(Sx)
+                Sy = np.random.normal(weed.Yp, sigmaiter, lenTx - 2)
+                #print(Sy)
+                len_S = Sx.size
+                #Xp = reduce((lambda x, y: x+y), Sx)//len(Sx)
+                #Yp = reduce((lambda x, y: x+y), Sy)//len(Sy)
+                Xp = np.sum(Sx) // len_S
+                Yp = np.sum(Sy) // len_S
                 seed = Seed(Sx, Sy, Xp, Yp)
                 seedlist.append(seed)
-                #Germination
+            # Germination
             for sd in seedlist:
                 weed = Weed(sd.Sx, sd.Sy, sd.Xp, sd.Yp, 0)
                 fitness = get_fitness(Tx, Ty, weed)
-                Rx = Tx[0:len(Tx) - lenTx]
-                Ry = Ty[0:len(Ty) - lenTy]
-                Tx = Tx[0:len(Tx) - len(Rx)]
-                Ty = Ty[0:len(Ty) - len(Ry)]
+                #Rx = Tx[0:len(Tx) - lenTx]
+                #Ry = Ty[0:len(Ty) - lenTy]
+                #Tx = Tx[0:len(Tx) - len(Rx)]
+                #Ty = Ty[0:len(Ty) - len(Ry)]
+                temp_len = Tx.size
+                Rx = Tx[0:temp_len - lenTx]
+                Ry = Ty[0:temp_len - lenTy]
+                len_Rx = Rx.size
+                Tx = Tx[0:temp_len - len_Rx]
+                Ty = Ty[0:temp_len - len_Rx]
                 weed.fitness = fitness
                 weedlist.append(weed)
         # Competetive Exclusion
@@ -101,70 +127,91 @@ def iwo_algorithm(Tx,Ty,weedlist,itermax,pmax,Smax,Smin,sigmafinal,sigmainit,n):
             competitive_exclusion(weedlist)
    return weedlist
 
-def iwo_test(Tx,Ty):
-    lenTx = len(Tx)
-    lenTy = len(Ty)
+def iwo_test(Tx,Ty, lenTx, lenTy):
+    #lenTx = Tx.size
+    #lenTy = lenTx
     weedlist=[]
     for i in range(10):
-        Wx = gd.get_xdata(0, 500, 8)
-        Wy = gd.get_ydata(0, 500, 8)
-        Xp = reduce((lambda x,y: x+y),Wx)//len(Wx)
-        Yp = reduce((lambda x,y: x+y),Wy)//len(Wy)
-        weed = Weed(Wx,Wy,Xp,Yp,0)
+        Wx = gd.get_xdata(0, 500, lenTx - 2)
+        Wy = gd.get_ydata(0, 500, lenTx - 2)
+        len_W = Wx.size
+        #Xp = reduce((lambda x,y: x+y),Wx)//len(Wx)
+        #Yp = reduce((lambda x,y: x+y),Wy)//len(Wy)
+        Xp = np.sum(Wx) // len_W
+        Yp = np.sum(Wy) // len_W
+        weed = Weed(Wx, Wy, Xp, Yp, 0)
         weedlist.append(weed)
     # iwo_algorithm(Tx,Ty,weedlist,itermax,pmax,Smax,Smin,sigmafinal,sigmainit,n)
-    wlist = iwo_algorithm(Tx,Ty,weedlist,5,150,10,1,0.01,1,3)
-    fmax = get_max_fitness(wlist)
-    for weed in wlist:
+    wlist = iwo_algorithm(Tx, Ty, weedlist, 15, 150, 10, 1, 0.01, 1, 3)
+    fmax, bestweed = get_max_fitness(wlist)
+    '''for weed in wlist:
         if fmax == weed.fitness:
             bestweed = weed
-            break
-    Rx = Tx[0:len(Tx) - lenTx]
-    Ry = Ty[0:len(Ty) - lenTy]
-    Tx = Tx[0:len(Tx) - len(Rx)]
-    Ty = Ty[0:len(Ty) - len(Ry)]
-    print("X-Coordinates of best weed",list(map(lambda x: math.ceil(x),bestweed.Wx)))
-    print("Y-Coordinates of best weed",list(map(lambda y: math.ceil(y),bestweed.Wy)))
+            break'''
+    #Rx = Tx[0:len(Tx) - lenTx]
+    #Ry = Ty[0:len(Ty) - lenTy]
+    #Tx = Tx[0:len(Tx) - len(Rx)]
+    #Ty = Ty[0:len(Ty) - len(Ry)]
+    temp_len = Tx.size
+    Rx = Tx[0:temp_len - lenTx]
+    Ry = Ty[0:temp_len - lenTy]
+    len_Rx = Rx.size
+    Tx = Tx[0:temp_len - len_Rx]
+    Ty = Ty[0:temp_len - len_Rx]
+    #print("X-Coordinates of best weed",list(map(lambda x: math.ceil(x),bestweed.Wx)))
+    #print("Y-Coordinates of best weed",list(map(lambda y: math.ceil(y),bestweed.Wy)))
     count = 0
-    bestweed.Wx = list(map(lambda x: math.ceil(x),bestweed.Wx))
-    bestweed.Wy = list(map(lambda y: math.ceil(y),bestweed.Wy))
-    for i in range(len(bestweed.Wx)):
+    #bestweed.Wx = list(map(lambda x: math.floor(x),bestweed.Wx))
+    #bestweed.Wy = list(map(lambda y: math.floor(y),bestweed.Wy))
+    bestweed.Wx=  bestweed.Wx.astype(int)
+    bestweed.Wy = bestweed.Wy.astype(int)
+    print("X-Coordinates of best weed", bestweed.Wx)
+    print("Y-Coordinates of best weed", bestweed.Wy)
+    Wxnew = np.sum(bestweed.Wx)//Wx.size
+    #print(Wxnew)
+    Wynew = np.sum(bestweed.Wy)//Wy.size
+    #print(Wynew)
+    #for i in range(len(bestweed.Wx)):
+    '''for i in range(bestweed.Wx.size):
         if bestweed.Wx[i] < min(Tx) or bestweed.Wx[i] > max(Tx):
             continue
         if bestweed.Wy[i] < min(Ty) or bestweed.Wy[i] > max(Ty):
             continue
-
-        Tx.append(bestweed.Wx[i])
-        Ty.append(bestweed.Wy[i])
-        count = count + 1
+        #Tx.append(bestweed.Wx[i])
+        #Ty.append(bestweed.Wy[i])
+        Tx = np.append(Tx, bestweed.Wx[i])
+        Ty = np.append(Ty, bestweed.Wy[i])
+        count = count + 1'''
+    Tx = np.append(Tx, Wxnew)
+    Ty = np.append(Ty, Wynew)
+    count = count + 1
     print("Updated X Coordinates", Tx)
     print("Updated Y Coordinates", Ty)
     distancevector = gd.get_distancevector(Tx, Ty)
     mst = pa.mst_prim(distancevector)
     mst_size = pa.get_tree(distancevector)
     print("Size of Steiner Tree for IWO", mst_size)
-    pa.draw_gridgraph(Tx, Ty, mst,lenTx,lenTy)
-    Tx = Tx[0:len(Tx) - count]
-    Ty = Ty[0:len(Ty) - count]
-    print("Restored X Coordinates=", Tx)
-    print("Restored Y Coordinates=", Ty)
+    pa.draw_gridgraph(Tx, Ty, mst, lenTx, lenTy)
+    Tx = Tx[0:Tx.size - count]
+    Ty = Ty[0:Ty.size - count]
+    print("Restored X Coordinates =", Tx)
+    print("Restored Y Coordinates =", Ty)
     print("End of IWO")
 
-Tx = gd.get_xdata(0,500,10)
-Ty = gd.get_ydata(0,500,10)
-print(Tx)
-print(Ty)
-lenTx = len(Tx)
-lenTy = len(Ty)
-print("X Coordinates", Tx)
-print("Y Coordinates", Ty)
-distancevector = gd.get_distancevector(Tx, Ty)
-mst = pa.mst_prim(distancevector)
-mst_size = pa.get_tree(distancevector)
-print("Size of the MST = ", mst_size)
-pa.draw_gridgraph(Tx, Ty, mst, lenTx, lenTy)
-iwo_test(Tx,Ty)
+if __name__ == '__main__':
 
-
-
-
+    '''Tx = gd.get_xdata(0, 500, 20)
+    Ty = gd.get_ydata(0, 500, 20)'''
+    Tx, Ty = np.load("terminal_point_{}_{}.npy".format(str(500), str(10)))
+    print(Tx)
+    print(Ty)
+    lenTx = Tx.size
+    lenTy = lenTx
+    print("X Coordinates", Tx)
+    print("Y Coordinates", Ty)
+    distancevector = gd.get_distancevector(Tx, Ty)
+    mst = pa.mst_prim(distancevector)
+    mst_size = pa.get_tree(distancevector)
+    print("Size of the MST = ", mst_size)
+    pa.draw_gridgraph(Tx, Ty, mst, lenTx, lenTy)
+    iwo_test(Tx, Ty, lenTx, lenTy)
--- pio_pso_demo.py
+++ pio_pso_demo.py
@@ -1,24 +1,75 @@
 import prim_algorithm as pa
 import gridgraphdemo as gd
+import invasive_weed_optimization as iwo
 import pigeon_optimization_algorithm as poa
 import particle_swarm_optimization as pso
 import constriction_factor_pso as cpso
+import numpy as np
+import time as t
 
-Tx = gd.get_xdata(0, 500, 20) #Change values over here and test
-Ty = gd.get_ydata(0, 500, 20) #Change values over here and test
-lenTx = len(Tx)
-lenTy = len(Ty)
+#Tx = gd.get_xdata(0, 500, 20)
+#Ty = gd.get_ydata(0, 500, 20)
+#lenTx = len(Tx)
+#lenTy = len(Ty)
+dim = 500
+n = 10
+Tx, Ty = np.load('terminal_point_{}_{}.npy'.format(str(dim), str(n)))
+lenTx = Tx.size
+lenTy = lenTx
 
-print("X Coordinates", Tx)
-print("Y Coordinates", Ty)
+#print("X Coordinates", Tx)
+#print("Y Coordinates", Ty)
 distancevector = gd.get_distancevector(Tx, Ty)
 mst = pa.mst_prim(distancevector)
 mst_size = pa.get_tree(distancevector)
 print("Size of the MST = ", mst_size)
 pa.draw_gridgraph(Tx, Ty, mst,lenTx,lenTy)
 
-poa.call_methods(Tx,Ty,lenTx,lenTy)
+data_pio = dict()
+data_pso = dict()
+data_cpso = dict()
 
-pso.call_methods(Tx,Ty,lenTx,lenTy)
+for i in range(100):
+    print('Iteration No :', i)
+    #print('PIO')
+    t1 = t.time()
+    res = poa.call_methods(Tx,Ty,lenTx,lenTy)
+    t2 = t.time()
+    #print('PSO')
+    data_pio[res[0]] = (res[1], res[2], t2-t1)
+    t2 - t.time()
+    res = pso.call_methods(Tx,Ty,lenTx,lenTy)
+    t3 = t.time()
+    #print('CPSO')
+    data_pso[res[0]] = (res[1], res[2], t3-t2)
+    t3 = t.time()
+    res = cpso.call_methods(Tx,Ty,lenTx,lenTy)
+    t4 = t.time()
+    data_cpso[res[0]] = (res[1], res[2], t4-t3)
+    '''iwo.iwo_test(Tx, Ty, lenTx, lenTy)
+    t5 = t.time()'''
 
-cpso.call_methods(Tx,Ty,lenTx,lenTy)
+min_pio = min(data_pio.keys())
+min_pso = min(data_pso.keys())
+min_cpso = min(data_cpso.keys())
+
+print('PIO Min Wt :', min_pio)
+print('X Coordinates :', data_pio[min_pio][0])
+print('Y Coordinates :', data_pio[min_pio][1])
+print('Time Required :', data_pio[min_pio][2])
+print('Error Ratio :', min_pio/mst_size)
+print('PSO Min Wt :', min_pso)
+print('X Coordinates :', data_pso[min_pso][0])
+print('Y Coordinates :', data_pso[min_pso][1])
+print('Time Required :', data_pso[min_pso][2])
+print('Error Ratio :', min_pso/mst_size)
+print('CPSO Min Wt :', min_cpso)
+print('X Coordinates :', data_cpso[min_cpso][0])
+print('Y Coordinates :', data_cpso[min_cpso][1])
+print('Time Required :', data_cpso[min_cpso][2])
+print('Error Ratio :', min_cpso/mst_size)
+
+'''print('Time Pegion Inspired Optimisation :', t2 - t1)
+print('Time Particle Swarm Optimisation :', t3 - t2)
+print('Time Constricted Particle Swarm Optimisation :', t4 - t3)
+print('Time Invasive Weed Optimisation :', t5 - t4)'''
